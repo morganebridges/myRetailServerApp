1682ae16000dadd25ef2022c9aff56089775547a
morgan.bridges@fjorgedigital.com <morgan.bridges@fjorgedigital.com>
2017-01-01 05:54:21 -0600

    added controller routes

--- src/main/java/myRetail/controller/ProductController.java
+++ src/main/java/myRetail/controller/ProductController.java
@@ -1,6 +1,7 @@
 package myRetail.controller;
 
 import myRetail.repository.ProductRepository;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import myRetail.model.Product;
@@ -10,30 +11,69 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * Created by fjorgeDevelopers on 12/28/16.
  */
 @EnableAutoConfiguration
-@RequestMapping(path = "/product")
+@RequestMapping(path = "/products")
 @RestController
 public class ProductController {
+
     @Autowired
     ProductRepository pRepo;
+
+    @RequestMapping(path="/")
+    public ResponseEntity<Product> getProduct(@RequestBody String id, HttpServletRequest request, HttpServletResponse response){
+        Product prod = pRepo.findById(id);
+        if(prod != null)
+            return new ResponseEntity<>(prod, HttpStatus.OK);;
+        return new ResponseEntity<>(prod, HttpStatus.OK);
+    }
+
+    @RequestMapping(path="/")
+    public ResponseEntity<List<Product>> getProducts(@RequestBody List<String> ids, HttpServletRequest request, HttpServletResponse response){
+        List<Product> returnList = new ArrayList<>();
+        ids.forEach(
+                id -> {
+                    Product itProd = pRepo.findById(id);
+                    if(itProd != null)
+                        returnList.add(itProd);
+                }
+        );
+        return new ResponseEntity<>(returnList, HttpStatus.OK);
+    }
+
     @RequestMapping(path = "/hello", method=RequestMethod.POST)
-    String hello(@RequestBody String name) {
+    String hello(@RequestBody String name ) {
         return "Hello, " + name + "!";
     }
-    @RequestMapping(path="gettest", method=RequestMethod.GET)
-    Product getProductTest(){
-        Product prod = new Product("Shoes", 34.12);
-        Product prod2 = pRepo.save(prod);
 
-        return prod2;
+    @RequestMapping(path="/getlist", method=RequestMethod.GET)
+    ResponseEntity<List<Product>> getProductTest(HttpServletRequest request, HttpServletResponse response){
+        List<Product> prod2 = pRepo.findAll();
+        if(prod2 != null)
+            return new ResponseEntity<>(prod2, HttpStatus.OK);
+        return new ResponseEntity<>(prod2, HttpStatus.BAD_REQUEST);
     }
-    @RequestMapping(path="update", method = RequestMethod.POST)
+
+    @RequestMapping(path="/add", method=RequestMethod.GET)
+    ResponseEntity<Product> insertProduct(Product insertProd, HttpServletRequest request, HttpServletResponse response){
+        Product returnProd = pRepo.insert(insertProd);
+        if(returnProd != null)
+            return new ResponseEntity<Product>(returnProd, HttpStatus.OK);
+        return new ResponseEntity<Product>(returnProd, HttpStatus.BAD_REQUEST);
+    }
+
+    @RequestMapping(path="/update", method = RequestMethod.POST)
     Product updateProduct(@RequestBody Product product){
         System.out.println(product.toString());
         String id = product.id;
@@ -45,4 +85,5 @@ public class ProductController {
         System.out.println(savedProd.toString());
         return savedProd;
     }
+
 }
